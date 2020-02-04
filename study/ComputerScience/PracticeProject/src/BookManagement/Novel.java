ca893917b134cc96e0c0643a451ca33a3a48ed3d
hoho00 <jhoya963@gmail.com>
2020-02-04 09:32:42 +0900

    origin

--- study/ComputerScience/PracticeProject/src/BookManagement/Novel.java
+++ study/ComputerScience/PracticeProject/src/BookManagement/Novel.java
@@ -1,5 +1,10 @@
 package BookManagement;
 
 public class Novel extends Book {
-
+	public int getLateFees(int lateDays) {
+		int lateFees;
+		int fee = 300;
+		lateFees = lateDays * fee; 
+		return lateFees;
+	}
 }
